6dc09ceead1495239045863650e8424f91851657
Jose Burgardt <joseburg89@hotmail.com>
2012-03-15 17:12:22 -0300

    first commit

---
+++ Script.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function() {
+	alert("Listo!");
+});
+
+
+$("#animation").keydown(function(e){
+        var KEYCODE_ESC = 27;
+        if(e.keyCode == KEYCODE_ESC){
+            $.mobile.changePage("#principal");
+        }
+    });
+
+$("#buttonday1").click(function(event) {
+	
+        event.preventDefault(); 
+        
+        var param = $("#alias").val();
+        url = "http://localhost/day1/api/dispatcher.php";
+        
+	// Para probar el punto 7 poner mal la url
+        // url = "http://localhost/day/api/dispatcher.php";
+        
+        $.post(url, {service: 'welcome.hello', params: {name: param}},
+		function(data){
+                    //alert(data);
+                    // $("#response").css("background","red");
+                    $("#response").html(data);
+                    
+		});
+});
+
+$("#msg").ajaxError(function(event, request, settings){
+  $("#response").hide();  
+  $(this).append("Error requesting page " + settings.url);
+  $("#msg").css("background","red");
+});
